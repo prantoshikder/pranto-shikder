42494fdd60a74bfab00bf0ee5f2612474f4cd775
prantoshikder <prantoshikder24@gmail.com>
2024-02-15 18:14:33 +0600

    Project initialize

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
+// @ts-check
+import withPlaiceholder from "@plaiceholder/next";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    domains: [
+      "/",
+      "http://",
+      "flagcdn.com",
+      "upload.wikimedia.org",
+      // "dev-user.localbet.xyz",
+    ],
+    formats: ["image/avif", "image/webp"],
+  },
+  output: "standalone",
+  distDir: ".next",
+};
+
+// module.exports = withPlaiceholder(nextConfig);
+export default withPlaiceholder(nextConfig);
